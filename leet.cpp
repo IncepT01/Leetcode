b7ad19ca3ae1340303a59d0d3fdefd706ee28a60
IncepT01 <tiszagergo10@gmail.com>
2025-01-27 22:43:58 +0100

    linked list update

--- leet.cpp
+++ leet.cpp
@@ -1,59 +1,57 @@
 #include<iostream>
 #include <list>
 
+
+ //* Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        std::list<int> nums;
-
-        while(x > 0){
-            int rem = x % 10;
-            nums.push_back(rem);
-            x = x - rem;
-            x = x/10;
-        }
-
-        int n = nums.size();
-
-
-        for(int num : nums){
-            std::cout<<num<<" ";
-        }
-
-
-        if(n > 0){
-            std::list<int>::iterator it = nums.begin();
-            std::list<int>::reverse_iterator bit = nums.rbegin();
-
-
-
-            for(int i = 0; i < n/2; i++){
-
-                //std::cout<<*it<<std::endl;
-                //std::cout<<*bit<<std::endl;
-                if(*it != *bit){
-                    return false;
-                }
-
-                ++it;
-                ++bit;
-            }
-
-            return true;
-        }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-
-        return false;
     }
-
 };
 
 
 int main() {
     Solution sol;
     int number = 100;  // Test input
-    bool result = sol.isPalindrome(number);
-    std::cout << "Is the number " << number << " a palindrome? " << (result ? "Yes" : "No") << std::endl;
+
+    ListNode ln1 = ListNode();
+    ln1.val = 1;
+    ListNode ln2 = ListNode();
+    ln2.val = 4;
+    ListNode ln3 = ListNode();
+    ln3.val = 7;
+
+    ListNode ln4 = ListNode();
+    ln4.val = 2;
+    ListNode ln5 = ListNode();
+    ln5.val = 10;
+    ListNode ln6 = ListNode();
+    ln6.val = 11;
+
+    ln1.next = &ln2;
+    ln2.next = &ln3;
+
+    ln4.next = &ln5;
+    ln5.next = &ln6;
+    
+    bool result = sol.mergeTwoLists(&ln1, &ln4);
+
+    ListNode* tmp = &ln1;
+    while (tmp != NULL)
+    {
+        std::cout<< (*tmp).val << std::endl;
+        tmp = (*tmp).next;
+    }
+    
     
     return 0;
 }
