43dba3f13c1230dc9d64b4acd95925eccd3452df
IncepT01 <tiszagergo10@gmail.com>
2025-02-08 17:11:19 +0100

    Merge Two Lists

--- leet.cpp
+++ leet.cpp
@@ -14,7 +14,28 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
+        ListNode e;
+        ListNode* head_ptr = &e;
+
+        while(list1->next != NULL && list2->next != NULL){
+            if(list1->val <= list2->val){
+                e.val = list1->val;
+                list1 = list1->next;
+            }
+            else{
+                e.val = list2->val;
+                list2 = list2->next;
+            }
+
+            ListNode tmp = ListNode();
+            e.next = &tmp;
+
+            e = tmp;
+        }
+
+
+
+        return head_ptr;
     }
 };
 
@@ -26,30 +47,45 @@ int main() {
     ListNode ln1 = ListNode();
     ln1.val = 1;
     ListNode ln2 = ListNode();
-    ln2.val = 4;
+    ln2.val = 2;
     ListNode ln3 = ListNode();
-    ln3.val = 7;
+    ln3.val = 4;
 
     ListNode ln4 = ListNode();
-    ln4.val = 2;
+    ln4.val = 1;
     ListNode ln5 = ListNode();
-    ln5.val = 10;
+    ln5.val = 3;
     ListNode ln6 = ListNode();
-    ln6.val = 11;
+    ln6.val = 4;
 
     ln1.next = &ln2;
     ln2.next = &ln3;
 
     ln4.next = &ln5;
     ln5.next = &ln6;
-    
-    bool result = sol.mergeTwoLists(&ln1, &ln4);
 
     ListNode* tmp = &ln1;
     while (tmp != NULL)
     {
-        std::cout<< (*tmp).val << std::endl;
+        std::cout<< (*tmp).val << ", ";
         tmp = (*tmp).next;
+    }
+    std::cout<<std::endl;
+
+    tmp = &ln4;
+    while (tmp != NULL)
+    {
+        std::cout<< (*tmp).val << ", ";
+        tmp = (*tmp).next;
+    }
+    std::cout<<std::endl;
+    
+    ListNode* result = sol.mergeTwoLists(&ln1, &ln4);
+
+    while (result != NULL)
+    {
+        std::cout<< result->val << std::endl;
+        result = result->next;
     }
     
     
