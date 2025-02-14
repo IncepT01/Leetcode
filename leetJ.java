80d8ea79288664d58d0e0aa0028ddda2e4ff067e
IncepT01 <tiszagergo10@gmail.com>
2025-02-14 19:09:42 +0100

    Setup done

--- leetJ.java
+++ leetJ.java
@@ -1,9 +1,14 @@
-class Solution {
+class Main {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
     }
 
     public static void main(String[] args){
-        System.out.println("It works!");
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        Main m = new Main();
+
+        m.merge(nums1, 3, nums2, 3);    
     }
 }
