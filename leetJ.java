3be33397dcf3eda1b8becd9faced890bdd49dee5
IncepT01 <tiszagergo10@gmail.com>
2025-02-15 23:33:56 +0100

    Base loop corrected

--- leetJ.java
+++ leetJ.java
@@ -3,17 +3,27 @@ class Main {
         int[] sorted = new int[m+n];
 
         int idx = 0;
+        int nIdx = 0;
+        int mIdx = 0;
 
-        while (idx < n && idx< m) {
+        while (nIdx < n && mIdx < m) {
             if(nums1[idx] <= nums2[idx]){
-                sorted[idx] = nums1[idx];
+                sorted[idx] = nums1[nIdx];
+                nIdx++;
             }
             else{
-                sorted[idx] = nums2[idx];
+                sorted[idx] = nums2[mIdx];
+                mIdx++;
             }
         }
 
-        
+        //Ther are still items in nums2
+        if(idx >= n && idx < m){
+
+        }
+        else if(idx >= n && idx < m){
+
+        }
     }
 
     public static void main(String[] args){
