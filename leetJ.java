6173c6aa292efd08c869485cb6591d2cbfabdc24
IncepT01 <tiszagergo10@gmail.com>
2025-02-16 20:10:56 +0100

    first solution

--- leetJ.java
+++ leetJ.java
@@ -1,6 +1,24 @@
 class Main {
     public int removeElement(int[] nums, int val) {
-        return -1;
+
+        int[] res = new int[nums.length];
+        int numIdx = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+                cnt++;               
+            }
+            else{
+                nums[numIdx] = nums[i];
+                numIdx++;
+            }
+        }
+
+        for(int i = 0; i < numIdx; i++){
+            nums[i] = res[i];
+        }
+        return cnt;
     }
 
     public static void main(String[] args){
@@ -11,7 +29,7 @@ class Main {
 
         m.removeElement(nums1, 3); 
         
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < nums1.length; i++){
             System.out.print(nums1[i]);
         }
     }
