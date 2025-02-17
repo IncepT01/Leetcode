dc175e3e550514ede7f2969a56d2e1a49cf381b9
IncepT01 <tiszagergo10@gmail.com>
2025-02-17 13:42:49 +0100

    setup

--- leetJ.java
+++ leetJ.java
@@ -1,24 +1,6 @@
 class Main {
-    public int removeElement(int[] nums, int val) {
-
-        int[] res = new int[nums.length];
-        int numIdx = 0;
-        int cnt = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == val){
-                cnt++;               
-            }
-            else{
-                res[numIdx] = nums[i];
-                numIdx++;
-            }
-        }
-
-        for(int i = 0; i < numIdx; i++){
-            nums[i] = res[i];
-        }
-        return nums.length - cnt;
+    int strStr(string haystack, string needle) {
+        
     }
 
     public static void main(String[] args){
@@ -27,11 +9,13 @@ class Main {
 
         Main m = new Main();
 
-        int a = m.removeElement(nums1, 2); 
+        int a = m.strStr("sadbutsad", "sad");
         
         System.out.println(a);
+        /* 
         for(int i = 0; i < nums1.length; i++){
             System.out.print(nums1[i]);
         }
+        */
     }
 }
