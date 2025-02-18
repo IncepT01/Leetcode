f764f1332ceafc4130ac3507ed71d44e52899044
IncepT01 <tiszagergo10@gmail.com>
2025-02-18 14:05:01 +0100

    Happy Number working(slow)

--- leet.py
+++ leet.py
@@ -1,1 +1,46 @@
-print("Hello World")
+import math
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        hs = set()
+
+        while(True):
+            digits = self.getDigits(n)
+            sum = 0
+            for i in digits:
+                sum += i*i
+
+            if sum == 1:
+                return True
+            
+            b = sum in hs
+            if b == True:
+                return False
+            else:
+                hs.add(sum)
+
+            n = sum
+
+
+        return True
+    
+
+    def getDigits(self, n):
+        i = 1
+        res = []
+        while n > 0 :
+            digit = n % 10
+            res.append(digit)
+            n = math.floor(n/10)
+            
+
+        return res
+
+
+sol = Solution()
+print(sol.isHappy(19))
