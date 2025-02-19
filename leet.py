2cc3fe322815f81979e041fe00f8f3e34336478e
IncepT01 <tiszagergo10@gmail.com>
2025-02-19 23:41:37 +0100

    Rem Dup Elem inital commit

--- leet.py
+++ leet.py
@@ -1,51 +1,24 @@
 import math
 
 class Solution(object):
-    def isHappy(self, n):
+    def removeDuplicates(self, nums):
         """
-        :type n: int
-        :rtype: bool
+        :type nums: List[int]
+        :rtype: int
         """
+        s = set()
 
-        if(n==1 or n==7):
-            return True
-        if(n<10):
-            return False
 
-        hs = set()
-
-        while(True):
-            digits = self.getDigits(n)
-            sum = 0
-            for i in digits:
-                sum += i*i
-
-            if sum == 1:
-                return True
-            
-            b = sum in hs
-            if b == True:
-                return False
+        for i in range(len(nums)):
+            print(s)
+            if nums[i] in s:
+                nums.pop(i)
+                i = i - 1
             else:
-                hs.add(sum)
-
-            n = sum
-
-
-        return True
-    
-
-    def getDigits(self, n):
-        i = 1
-        res = []
-        while n > 0 :
-            digit = n % 10
-            res.append(digit)
-            n = n//10
-            
-
-        return res
+                s.add(nums[i])
 
 
 sol = Solution()
-print(sol.isHappy(19))
+arr = [0,0,1,1,1,2,2,3,3,4]
+sol.removeDuplicates(arr)
+print(arr)
